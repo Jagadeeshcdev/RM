5cb85e847453365d84ce0e34c83d4bd8fb5f909f
Jagadeeshcdev <89693846+Jagadeeshcdev@users.noreply.github.com>
2024-07-09 13:56:15 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ pip install pandas
 pip install plotly
 pip install seaborn
 pip install matplotlib
-pip install wordcloud
 pip install python-dateutil
 pip install google-auth
 pip install google-auth-oauthlib
